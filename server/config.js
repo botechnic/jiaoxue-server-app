8b802b231a79133ceebdaeaff4af0bbdf337649e
botechnic <maxuejun@botechnic.com>
2015-01-23 16:11:57 +0800

    配置路径修改

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 var config = module.exports;
 
-config.prev_path = '/home/pony/jiaoxue/jiaoxue-server-app/public/data/';
+config.prev_path = '/home/pony/jiaoxue/jiaoxue-server-app/client/data/';
 config.live_addr = 'rtmp://192.168.10.250:1935/live';
 config.ffmpeg_path = '/home/pony/simple-rtmp-server-master/trunk/objs/ffmpeg/bin/ffmpeg';
